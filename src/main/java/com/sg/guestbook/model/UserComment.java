e38d58b154775cb39bb909874c75b5ec1a2c7bf9
subhrajit <subhrajit.banerjee.official>
2021-01-23 15:22:42 -0500

    Added default constructors

--- src/main/java/com/sg/guestbook/model/UserComment.java
+++ src/main/java/com/sg/guestbook/model/UserComment.java
@@ -1,7 +1,9 @@
 package com.sg.guestbook.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserComment {
 
     @Id
